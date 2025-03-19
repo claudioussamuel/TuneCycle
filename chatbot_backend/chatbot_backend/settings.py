5d8484c47e370e56e561fa2b465cb2a770b9b849
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-03-19 09:16:35 +0000

    Check cause

--- chatbot_backend/chatbot_backend/settings.py
+++ chatbot_backend/chatbot_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u45)wt(5r%%y^oe!n*99qk-und59mzfy+25bk9vhgi=#&d2_v1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
