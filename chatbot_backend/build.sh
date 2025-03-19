7ced50ec308f714dace26ed20d76c6b8d404c706
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-03-19 03:37:23 +0000

    revamp

--- chatbot_backend/build.sh
+++ chatbot_backend/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -o errexit
+
+
+cd chat_backend
+
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Install system dependencies if needed (optional)
-# apt-get update && apt-get install -y some-system-library
-
-echo "Build complete!"
-
